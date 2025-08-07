aa300214364a6d370591607e08d9c7b86ab4d636
Gustavo Santos <santosgustavo52579@gmail.com>
2025-08-07 20:25:23 -0300

    Sincronização do projeto par ou impar

---
+++ 03_DesvioCondicional/ParOuImpar.cs
@@ -0,0 +1,18 @@
+using System 
+
+class ParOuImpar
+{
+	static void Main()
+	{
+		int numero;
+		
+		Console.WriteLine("Digite um número");
+		numero = int.Parse Console.RedLine
+		
+		if (numero %2 == 0)
+			Console.WriteLine("O número é ´par. ");
+		else
+			Console.WriteLine("o número é impar. ");
+	}
+}
+		
