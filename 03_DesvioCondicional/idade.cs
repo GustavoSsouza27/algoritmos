5c5e69c3da9b7acfd6d31077efc42fa37e2ba5b2
Gustavo Santos <santosgustavo52579@gmail.com>
2025-08-07 21:48:19 -0300

    converção da atividade do portugol classificação de idade

---
+++ 03_DesvioCondicional/idade.cs
@@ -0,0 +1,33 @@
+using System ;
+
+class Idade
+{
+	static void Main()
+	{
+		int idade;
+		
+		Console.WriteLine("Digite sua idade: ");
+		idade = int.Parse (Console.ReadLine());
+		
+		if (idade <= 12)
+		{
+			Console.WriteLine("Você é criança");
+		}	
+		else if (idade <= 17)
+		{
+			Console.WriteLine("Você é Adolecente");
+		}
+		else if (idade <= 60)
+		{
+			Console.WriteLine("Você é adulto");
+		}
+		else
+		{
+			Console.WriteLine("Você é idoso");
+		}
+	}
+}
+		
+			
+			
+		
