6dd039e5b52f28a085fef1cb65bfb2ebcb6e9a52
Gustavo Santos <santosgustavo52579@gmail.com>
2025-08-08 22:13:33 -0300

    Tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            int multiplicador = int.Parse(Console.ReadLine());
+             
+            if (multiplicador >= 1 || multiplicador >= 10)
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{multiplicador}, x {i} = {multiplicador * i}");
+            }
+
+        }
+    }
+}
---
+++ 06_Tabuada/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("06_Tabuada")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06_Tabuada")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("98a9177d-65b5-4f0d-a338-c14f31acf272")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
